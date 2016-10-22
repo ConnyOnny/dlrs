a7a78b1d21e8791ea3825166e066f6b9e9b082aa
Constantin Berhard <git.mail.enormator@xoxy.net>
2016-10-22 23:36:42 +0200

    downloading http to stdout works

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+extern crate hyper;
+
+use std::io::Read;
+
+const BUFFER_SIZE : usize = 1024;
+
+fn main() {
+    let mut args = std::env::args();
+    let programname = args.next().expect("Program name not given as argument 0. This is not supported.");
+    let usagestring = format!("Usage: {} url [outfilename]\nIf no outfilename is given, will download to stdout", programname);
+    let url = args.next().expect(&usagestring);
+    let stdout = std::io::stdout();
+    let mut outwriter : Box<std::io::Write> = match args.next() {
+        Some(fname) => unimplemented!(),
+        None => Box::new(stdout.lock()),
+    };
+    if args.next().is_some() {
+        panic!("Too many arguments.");
+    }
+
+    let mut buffer = [0u8;BUFFER_SIZE];
+
+    // TODO if http
+    let client = hyper::client::Client::new();
+    let mut response = client.get(&url).send().unwrap();
+    loop {
+        let read_bytes = response.read(&mut buffer).expect("reading error");
+        if read_bytes == 0 {
+            break;
+        }
+        outwriter.write_all(&buffer[0..read_bytes]).expect("writing error");
+    }
+}
